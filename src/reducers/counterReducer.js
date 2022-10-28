4d7635059729d3ec9f0bc2ae487df5b2876a9870
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-28 16:15:58 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from "react";
 const counterReducer = (state,action) =>{
-  const initialState = { count: 0 };
   switch (action.type) {
     case "increment":
       return { count: state.count + 1 };
